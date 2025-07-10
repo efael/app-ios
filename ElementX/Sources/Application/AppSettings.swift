6b6bd2424f7fb132d88fe39212d80e05e696fba8
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-07-10 21:49:14 +0500

    chore: remove ability to choose servers

--- ElementX/Sources/Application/AppSettings.swift
+++ ElementX/Sources/Application/AppSettings.swift
@@ -159,7 +159,7 @@ final class AppSettings {
     ///
     /// Account provider is the friendly term for the server name. It should not contain an `https` prefix and should
     /// match the last part of the user ID. For example `example.com` and not `https://matrix.example.com`.
-    private(set) var accountProviders = ["efael.net", "efael.uz", "floss.uz"]
+    private(set) var accountProviders = ["efael.uz"]
     /// Whether or not the user is allowed to manually enter their own account provider or must select from one of `defaultAccountProviders`.
     private(set) var allowOtherAccountProviders = false
     
@@ -262,7 +262,7 @@ final class AppSettings {
     /// The configuration to use for analytics. Set to `nil` to disable analytics.
     let analyticsConfiguration: AnalyticsConfiguration? = nil // AppSettings.makeAnalyticsConfiguration()
     /// The URL to open with more information about analytics terms. When this is `nil` the "Learn more" link will be hidden.
-    private(set) var analyticsTermsURL: URL? = "https://element.io/cookie-policy"
+    private(set) var analyticsTermsURL: URL? = nil // "https://element.io/cookie-policy"
     /// Whether or not there the app is able ask for user consent to enable analytics or sentry reporting.
     var canPromptForAnalytics: Bool { analyticsConfiguration != nil || bugReportSentryURL != nil }
     
